2ef620c51b4f44422f9ae41c588e9a4f3383d31f
HaziqJefry26 <154412120+HaziqJefry26@users.noreply.github.com>
2023-12-21 14:14:25 +0800

    Update iris-generate-predict_ANNC.py

--- iris-generate-predict_ANNC.py
+++ iris-generate-predict_ANNC.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from tensorflow.keras.models import load_model
+import pickle
 #import seaborn as sns
 
 st.write("# Simple Iris Flower Prediction App")
@@ -25,7 +25,7 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-loaded_model = load_model("Iris_ANNC.h5") #rb: read binary
+loaded_model = pickle.load(open("Iris_ANNC.h5", "rb")) #rb: read binary
 #loaded_scaler = pickle.load(open("scaler_Iris.pkl", "rb"))
 
 new_pred = loaded_model.predict(df) # testing (examination)
